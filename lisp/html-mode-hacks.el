e57b7244f6f1d516f64750c882b3800e24618730
Robert S. Jones <robert.jones.sv@gmail.com>
2015-03-01 18:27:15 -0800

    Add helm projectile.

--- lisp/html-mode-hacks.el
+++ lisp/html-mode-hacks.el
@@ -1,5 +1,8 @@
 (defun html-mode-customizations()
-  (auto-fill-mode -1)
-  (remove-hook 'html-mode-hook #'turn-on-auto-fill))
+  (auto-fill-mode -1))
 
+(remove-hook 'html-mode-hook #'turn-on-auto-fill)
 (add-hook 'html-mode-hook 'html-mode-customizations)
+
+;; https://github.com/smihica/emmet-mode
+(add-hook 'html-mode-hook 'emmet-mode)
