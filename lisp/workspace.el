bcc091dcfbebe5355e4eb1078ed422102803d57c
Robert S. Jones <robert.jones.sv@gmail.com>
2015-03-21 09:55:10 -0700

    Hack in next/prev wrapping for perspective-mode navigation.

--- lisp/workspace.el
+++ lisp/workspace.el
@@ -19,3 +19,29 @@
 ;; match function
 (defalias 'helm-buffers-list--match-fn 'helm-buffers-match-function)
 (global-set-key (kbd "C-x b") 'helm-projectile-switch-to-buffer)
+
+;; Hack `persp-next' and `persp-prev' to wrap until my PR gets merged
+(defun persp-next-wrap ()
+  "Switch to next perspective (to the right).
+Wrap to the beginning if necessary."
+  (interactive)
+  (let* ((names (persp-names))
+         (pos (cl-position (persp-name persp-curr) names)))
+    (cond
+     ((null pos) (persp-find-some))
+     ((= pos (1- (length names))) (persp-switch (nth 0 names)))
+     (t (persp-switch (nth (1+ pos) names))))))
+
+(defun persp-prev-wrap ()
+  "Switch to previous perspective (to the left).
+Wrap to the beginning if necessary."
+  (interactive)
+  (let* ((names (persp-names))
+         (pos (cl-position (persp-name persp-curr) names)))
+    (cond
+     ((null pos) (persp-find-some))
+     ((= pos 0) (persp-switch (nth (1- (length names)) names)))
+     (t (persp-switch (nth (1- pos) names))))))
+
+(define-key persp-mode-map (kbd "C-x x n") 'persp-next-wrap)
+(define-key persp-mode-map (kbd "C-x x p") 'persp-prev-wrap)
