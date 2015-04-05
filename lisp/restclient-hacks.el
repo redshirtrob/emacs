3e8d75cfe00c3187c49d13a43075e943eafab749
Robert S. Jones <robert.jones.sv@gmail.com>
2015-04-04 21:35:24 -0700

    Restclient fixes:  - Use forked version until PR gets merged  - Add hook function to `restclient-http-do-hook'

--- lisp/restclient-hacks.el
+++ lisp/restclient-hacks.el
@@ -1,6 +1,8 @@
 ;; Hack so restclient-mode respects proxy settings.  This
 ;; only works on OS X.
 
+(require 'restclient)
+
 (defun networksetup-query (interface parameter)
   "Query the network setup for webproxy configuration."
   (let ((command (format "networksetup -getwebproxy \"%s\" | grep \"^%s\" | awk '{print $2}' | xargs echo -n" interface parameter)))
@@ -14,17 +16,13 @@
   (let ((enabled (networksetup-query interface "Enabled")))
     (if (equal enabled "No") nil t)))
 
-;; TODO: Update restclient to perform these checks before making a request,
-;;       rather than relying on the buffer local variable.  Proxy settings
-;;       could change during buffer's lifetime, but `url-proxy-settings'
-;;       is only updated on switching the major mode.
-(defun restclient-mode-customizations ()
-  (message "Entering REST Client mode")
-  (when (is-proxy-enabled "Wi-Fi")
+(defun restclient-http-do-customizations ()
+  (make-local-variable 'url-proxy-services)
+  (if (is-proxy-enabled "Wi-Fi")
     (let ((proxy-service (format "%s:%s" (proxy-server "Wi-Fi") (proxy-port "Wi-Fi"))))
-      (make-local-variable 'url-proxy-services)
-      (setq url-proxy-services `(("http" . ,proxy-service) ("https" . ,proxy-service))))))
+      (setq url-proxy-services `(("http" . ,proxy-service) ("https" . ,proxy-service))))
+    (setq url-proxy-services nil)))
 
-(add-hook 'restclient-mode-hook 'restclient-mode-customizations)
+(add-hook 'restclient-http-do-hook 'restclient-http-do-customizations)
 
 (setq auto-mode-alist (cons '("\\.api$" . restclient-mode) auto-mode-alist))
