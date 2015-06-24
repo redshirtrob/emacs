016590195be97cbf6fb7c64dafb3d207f3a77154
Robert S. Jones <robert.jones.sv@gmail.com>
2015-06-24 11:58:33 -0700

    Enable marmalade.

--- lisp/package-extensions.el
+++ lisp/package-extensions.el
@@ -1,7 +1,7 @@
 ;; Configure additional package archives
 (package-initialize)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;;                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
