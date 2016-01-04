9a3359ccffcc15dda79a80a46811581a28b62ffa
Robert S. Jones <rob@urx.com>
2016-01-04 14:23:58 -0800

    Update my package list

--- lisp/package-extensions.el
+++ lisp/package-extensions.el
@@ -18,9 +18,17 @@
 ;; http://batsov.com/articles/2012/02/19/package-management-in-emacs-the-good-the-bad-and-the-ugly/
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defvar prelude-packages
-  '(ack-and-a-half auto-complete autopair concurrent ctable dash deferred
-		   epc ipython popup python-django python-mode python-pylint
-		   pyvirtualenv s virtualenv yasnippet)
+  '(2048-game ack-and-a-half async auto-complete autopair caml cider cl-generic
+              clojure-mode company company-c-headers company-cmake company-go company-irony
+              company-jedi company-restclient concurrent ctable dash deferred diminish
+              editorconfig editorconfig-fnmatch editorconfig-core emmet-mode epc epl
+              flx flx-ido git-commit go-autocomplete go-mode go-snippets handlebars-mode
+              helm helm-ack helm-company helm-core helm-dash helm-git helm-projectile helm-themes
+              heroku iedit ipython irony jedi jedi-core magit magit-popup markdown-mode paredit
+              paredit-everywhere persp-mode persp-projectile perspective pkg-info popup projectile
+              python-django python-environment python-mode python-pylint pyvirtualenv queue
+              restclient know-your-http-well s seq slime slime-company spinner tuareg utop
+              virtualenv w3m web-mode with-editor yasnippet zencoding-mode)
   "A list of packages to ensure are installed at launch.")
 
 (defun prelude-packages-installed-p ()
