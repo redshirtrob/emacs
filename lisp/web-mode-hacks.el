821ce755cea4af3be500386944bd9744cfd13ba5
Robert S. Jones <robert@pinterest.com>
2016-05-11 09:56:17 -0700

    Use Django engine for nunjucks files

--- lisp/web-mode-hacks.el
+++ lisp/web-mode-hacks.el
@@ -1,7 +1,6 @@
 (require 'web-mode)
 
 (defun web-mode-customizations ()
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (setq web-mode-enable-part-face t)
   (set-face-attribute 'web-mode-html-tag-face nil :foreground "Pink3")
   (set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "Black")
@@ -11,7 +10,8 @@
   (set-face-attribute 'web-mode-part-face nil :foreground "Red")
   (setq web-mode-markup-indent-offset 2))
 
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.nunjucks?\\'" . web-mode))
+(setq web-mode-engines-alist '(("django" . "\\.nunjucks\\'")))
 (add-hook 'web-mode-hook 'web-mode-customizations)
 
-(setq auto-mode-alist
-      (cons '("\\.html$" . web-mode) auto-mode-alist))
