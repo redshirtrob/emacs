5a96e629cbfd9a4b39df16f482d40dec65685ef5
Robert S. Jones <robert@pinterest.com>
2016-05-11 10:02:24 -0700

    Remove old packages
    `editorconfig-fnmatch` and `editorconfig-core` are renamed to `editorconfig`.

--- lisp/package-extensions.el
+++ lisp/package-extensions.el
@@ -21,7 +21,7 @@
   '(2048-game ack-and-a-half async auto-complete autopair caml cider cl-generic
               clojure-mode company company-c-headers company-cmake company-go company-irony
               company-jedi company-restclient concurrent ctable dash deferred diminish
-              editorconfig editorconfig-fnmatch editorconfig-core emmet-mode epc epl
+              editorconfig  emmet-mode epc epl
               flx flx-ido git-commit go-autocomplete go-mode go-snippets handlebars-mode
               helm helm-ack helm-company helm-core helm-dash helm-git helm-projectile helm-themes
               heroku iedit ipython irony jedi jedi-core magit magit-popup markdown-mode paredit
