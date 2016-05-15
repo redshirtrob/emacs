051331157d92b57b9e1d73f81dd947391e66771c
Robert S. Jones <robert@pinterest.com>
2016-05-14 21:44:44 -0700

    Fix linter warnings

--- lisp/objc-mode-hacks.el
+++ lisp/objc-mode-hacks.el
@@ -1,3 +1,11 @@
+;;; objc-mode-hacks --- Customizations to objc-mode
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'objc-mode)
+
 (setq auto-mode-alist
       (cons '("\\.m$" . objc-mode) auto-mode-alist))
 (setq auto-mode-alist
@@ -6,11 +14,13 @@
       (cons '("\\.mm$" . objc-mode) auto-mode-alist))
 
 (defun objc-in-header-file ()
+  "Determine if the buffer is a header file."
   (let* ((filename (buffer-file-name))
          (extension (car (last (split-string filename "\\.")))))
     (string= "h" extension)))
 
 (defun objc-jump-to-extension (extension)
+  "Jump to a file with the given `EXTENSION'."
   (let* ((filename (buffer-file-name))
          (file-components (append (butlast (split-string filename
                                                          "\\."))
@@ -26,15 +36,19 @@ Assumes both files are in the same directory."
     (objc-jump-to-extension "h")))
 
 (defun insert-nsstring-constant (name)
-  "Insert an NSString constant"
+  "Insert an NSString constant with the given `NAME'."
   (interactive
    (let ((constant-name
           (read-string "Enter constant name: " (file-name-base (buffer-file-name)))))
      (insert (format "static NSString *const %s = @\"%s\";\n" constant-name constant-name)))))
 
 (defun objc-mode-customizations ()
+  "Customize `objc-mode'."
   (define-key objc-mode-map (kbd "C-c C-w") 'copy-word-under-cursor)
   (define-key objc-mode-map (kbd "C-c /") 'uncomment-region)
   (define-key objc-mode-map (kbd "C-c t") 'objc-jump-between-header-source))
 
 (add-hook 'objc-mode-hook 'objc-mode-customizations)
+
+(provide 'objc-mode-hacks)
+;;; objc-mode-hacks.el ends here
