8ddba229b1dd822f7ffbc21a32f64dd8dc11f3c2
Robert S. Jones <robert@pinterest.com>
2016-05-14 21:29:12 -0700

    Fix linter warnings

--- lisp/emacs-lisp-mode-hacks.el
+++ lisp/emacs-lisp-mode-hacks.el
@@ -1,6 +1,17 @@
+;;; emacs-lisp-mode-hacks --- Configure emacs-lisp
+
+;;; Commentary:
+
+;;; Code:
+
 (setq auto-mode-alist
       (cons '("emacs$" . emacs-lisp-mode) auto-mode-alist))
 
 (defun emacs-lisp-mode-customizations ()
+  "Customize `emacs-lisp-mode'."
+  (defvar copy-word-under-cursor-regex)
   (setq copy-word-under-cursor-regex "[^[:word:]\\-]"))
 (add-hook 'emacs-lisp-mode-hook 'emacs-lisp-mode-customizations)
+
+(provide 'emacs-lisp-mode-hacks)
+;;; emacs-lisp-mode-hacks.el ends here
