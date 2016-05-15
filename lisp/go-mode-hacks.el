da3ed3097c111806d66a7b1723d8e36de8ee3d4f
Robert S. Jones <robert@pinterest.com>
2016-05-14 21:32:16 -0700

    Fix linter warnings

--- lisp/go-mode-hacks.el
+++ lisp/go-mode-hacks.el
@@ -1,1 +1,12 @@
+;;; go-mode-hacks --- Configure Go
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'go-mode)
+
 (add-hook 'before-save-hook #'gofmt-before-save)
+
+(provide 'go-mode-hacks)
+;;; go-mode-hacks.el ends here
