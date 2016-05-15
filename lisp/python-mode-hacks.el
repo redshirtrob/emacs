25b663006e05bb677d5fa0a3682308914b27249f
Robert S. Jones <robert@pinterest.com>
2016-05-14 21:52:10 -0700

    Fix linter warnings

--- lisp/python-mode-hacks.el
+++ lisp/python-mode-hacks.el
@@ -1,9 +1,15 @@
-;; May need to eval (jedi:start-server)
+;;; python-mode-hacks --- Customizations to python-mode
+
+;;; Commentary:
+;;; May need to eval (jedi:start-server)
+
+;;; Code:
 
 (require 'jedi)
 (jedi:start-server)
 
 (defun python-mode-hacks ()
+  "Configure `python-mode'."
   (add-to-list 'company-backends 'company-jedi)
   (local-set-key (kbd "C-]") 'jedi:complete))
 
@@ -13,3 +19,6 @@
 (setq py-load-pymacsp nil)
 
 (setq jedi:complete-on-dot t)
+
+(provide 'python-mode-hacks)
+;;; python-mode-hacks.el ends here
