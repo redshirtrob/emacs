dcae442a0a8216b42012838d27ee1e842d44c188
Robert S. Jones <robert@pinterest.com>
2016-05-14 22:04:44 -0700

    Fix linter warnings

--- lisp/ropemacs-hacks.el
+++ lisp/ropemacs-hacks.el
@@ -1,14 +1,19 @@
-;; Configure ropemacs
+;;; ropemacs-hacks --- Customizations to ropemacs
 
-;; Required pymacs which must be installed with the following on (OS X)
-;; $ pip install -e "git+https://github.com/pinard/Pymacs.git#egg=Pymacs" --src "/usr/local/lib/python2.7/site-packages"
-;; $ cd /usr/local/lib/python2.7/site-packages/pymacs
-;; $ make
+;;; Commentary:
+;;; Required pymacs which must be installed with the following on (OS X)
+;;; $ pip install -e "git+https://github.com/pinard/Pymacs.git#egg=Pymacs" --src "/usr/local/lib/python2.7/site-packages"
+;;; $ cd /usr/local/lib/python2.7/site-packages/pymacs
+;;; $ make
+
+;;; Code:
 
 (defun load-ropemacs ()
-  "Load pymacs and ropemacs"
+  "Load pymacs and ropemacs."
   (interactive)
 
+  (require 'ropemacs)
+  (require 'pymacs)
   ;; My "C-x p" conflicts with ropemacs, unset it here
   (global-set-key (kbd "C-x p") nil)
   
@@ -22,3 +27,6 @@
   ;; Automatically save project python buffers before refactorings
   (setq ropemacs-confirm-saving 'nil))
 (global-set-key "\C-xtl" 'load-ropemacs)
+
+(provide 'ropemacs-hacks)
+;;; ropemacs-hacks.el ends here
