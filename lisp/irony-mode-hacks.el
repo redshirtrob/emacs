2e34fd4d70634ac4c4e5ebcf77c1f539b0cd64db
Robert S. Jones <robert@pinterest.com>
2016-05-14 21:36:56 -0700

    Fix linter warnings

--- lisp/irony-mode-hacks.el
+++ lisp/irony-mode-hacks.el
@@ -1,3 +1,9 @@
+;;; irony-mode-hacks --- Configure irony mode
+
+;;; Commentary:
+
+;;; Code:
+
 ;; From https://github.com/Sarcasm/irony-mode
 
 ;; Installing on OS X
@@ -6,14 +12,20 @@
 ;; Run `irony-install-server' and alter cmake command as follows
 ;;  cmake -DCMAKE_PREFIX_PATH=/usr/local/opt/llvm -DCMAKE_INSTALL_PREFIX\=/Users/rob/.emacs.d/irony/ /Users/rob/.emacs.d/elpa/irony-20150408.1501/server && cmake --build . --use-stderr --config Release --target install
 
+(require 'irony-mode)
+
 (add-hook 'objc-mode-hook 'irony-mode)
 
 ;; replace the `completion-at-point' and `complete-symbol' bindings in
 ;; irony-mode's buffers by irony-mode's function
 (defun my-irony-mode-hook ()
+  "Configure `irony-mode'."
   (define-key irony-mode-map [remap completion-at-point]
     'irony-completion-at-point-async)
   (define-key irony-mode-map [remap complete-symbol]
     'irony-completion-at-point-async))
 (add-hook 'irony-mode-hook 'my-irony-mode-hook)
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+(provide 'irony-mode-hacks)
+;;; irony-mode-hacks.el ends here
