a59e55d8fc40fdaf70faa02499b2c60dbfd715d0
Robert S. Jones <robert@pinterest.com>
2016-05-14 20:06:22 -0700

    Fix linter warnings

--- lisp/c-mode-hacks.el
+++ lisp/c-mode-hacks.el
@@ -1,7 +1,19 @@
+;;; c-mode-hacks --- Configure c-mode
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cc-mode) ;; for objc-mode-map
+
 (setq-default c-basic-offset 4)
 
 (defun c-mode-customizations ()
+  "Customize `c-mode'."
   (define-key objc-mode-map (kbd "C-c C-w") 'copy-word-under-cursor)
   (define-key objc-mode-map (kbd "C-c /") 'uncomment-region))
 
 (add-hook 'c-mode-hook 'c-mode-customizations)
+
+(provide 'c-mode-hacks)
+;;; c-mode-hacks.el ends here
