bbdd70fde8492589998ff655eeed374ca6ace7ae
Robert S. Jones <robert@pinterest.com>
2016-05-14 22:06:43 -0700

    Fix linter warnings

--- lisp/text-mode-hacks.el
+++ lisp/text-mode-hacks.el
@@ -1,1 +1,11 @@
+;;; text-mode-hacks --- Customizations to text-mode
+
+;;; Commentary:
+
+;;; Code:
+
 (add-hook 'text-mode-hook 'auto-fill-mode)
+
+(provide 'text-mode-hacks)
+;;; text-mode-hacks.el ends here
+
