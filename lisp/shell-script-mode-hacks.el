b5f771e65c104a3de68835edb73c9492b9318d6d
Robert S. Jones <robert@pinterest.com>
2016-05-14 22:05:58 -0700

    Fix linter warnings

--- lisp/shell-script-mode-hacks.el
+++ lisp/shell-script-mode-hacks.el
@@ -1,2 +1,11 @@
+;;; shell-script-mode-hacks --- Customizations to shell-script-mode
+
+;;; Commentary:
+
+;;; Code:
+
 (setq auto-mode-alist
       (cons '("zshrc$" . shell-script-mode) auto-mode-alist))
+
+(provide 'shell-script-mode-hacks)
+;;; shell-script-mode-hacks.el ends here
