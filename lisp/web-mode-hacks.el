97627ed9b396cc8e0b05f14e9c759809cb23d007
Robert S. Jones <robert.jones.sv@gmail.com>
2016-10-23 21:19:45 -0700

    Some changes to make working with React/JSX nicer

--- lisp/web-mode-hacks.el
+++ lisp/web-mode-hacks.el
@@ -25,7 +25,16 @@
 
 ;; React Hacks
 (add-to-list 'auto-mode-alist '("\\react.*.js[x]?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))
 (setq web-mode-content-types-alist '(("jsx"  . "\\react.*.js[x]?\\'")))
+
+(add-hook 'web-mode-hook
+          (lambda ()
+            ;; short circuit js mode and just do everything in jsx-mode
+            (if (equal web-mode-content-type "javascript")
+                (web-mode-set-content-type "jsx")
+              (message "now set to: %s" web-mode-content-type))))
+(setq web-mode-code-indent-offset 2)
 
 (add-hook 'web-mode-hook 'web-mode-customizations)
 
