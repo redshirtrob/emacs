fc9abb675235c7394e7759edaba7306f8ce821f3
Robert S. Jones <robert.jones.sv@gmail.com>
2017-08-26 17:11:22 -0700

    Update to latest packages and fix resultant issues

--- lisp/package-extensions.el
+++ lisp/package-extensions.el
@@ -12,14 +12,7 @@
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
+(require 'el-get)
 (el-get 'sync)
 
 ;; Install missing packages
@@ -29,7 +22,7 @@
   '(2048-game ack-and-a-half ag async auto-complete autopair caml cider cl-generic
               clojure-mode company company-c-headers company-cmake company-go company-irony
               company-jedi company-restclient concurrent ctable dash deferred diminish dumb-jump
-              editorconfig  emmet-mode epc ensime epl erlang flycheck flycheck-elixir flymake-elixir flycheck-tip
+              editorconfig  emmet-mode epc ensime epl erlang flycheck flycheck-elixir flymake-elixir flycheck-popup-tip flycheck-tip
               flx flx-ido git-commit go-autocomplete go-mode go-snippets handlebars-mode
               helm helm-ack helm-company helm-core helm-dash helm-git helm-projectile helm-themes
               heroku iedit ipython irony jedi jedi-core magit magit-popup markdown-mode neotree paredit
@@ -40,7 +33,7 @@
               virtualenv w3m web-mode with-editor yasnippet zencoding-mode)
   "A list of packages to ensure are installed at launch.")
 
-(require 'cl-lib) ;; Note: if this causes issues, switch to (require 'cl)
+(require 'cl) ;; Note: if this causes issues, switch to (require 'cl)
 (defun local-packages-installed-p ()
   "Check if packages are installed."
   (loop for p in local-packages
